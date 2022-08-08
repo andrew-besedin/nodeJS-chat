6b81c7dbba5cb0d081ebcc21032e2c8595448ea8
andrewprog97 <andrewprog97@example.com>
2022-08-08 21:36:48 +0300

    initial commit

---
+++ assets/index.js
@@ -0,0 +1,22 @@
+const socket = io({ forceBase64: true });
+
+userNameInput = document.querySelector(".user-name-input");
+messageInput = document.querySelector(".message-input");
+
+document.querySelector("button").addEventListener("click", () => {
+    socket.emit("message-send", { 
+        message: messageInput.value, 
+        userName: userNameInput.value
+    });
+});
+
+socket.on("message-response", (data) => {
+    let tableRow = document.createElement("tr");
+    let tableData = document.createElement("td");
+    tableData.innerText = data.userName;
+    tableRow.appendChild(tableData);
+    tableData = document.createElement("td");
+    tableData.innerText = data.message;
+    tableRow.appendChild(tableData);
+    document.querySelector("table").appendChild(tableRow);
+});
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+app.use(express.static("assets"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile( __dirname + "/assets/index.html");
+});
+
+io.on("connection", async (socket) => {
+    console.log(socket);
+    const db = await open({filename: "./database/base.db", driver: sqlite3.Database});
+    (await db.all("SELECT * FROM messages"))?.forEach((element) => {
+        socket.emit("message-response", {
+            message: element.message,
+            userName: element.userName
+        });
+    });
+    await db.close();
+
+    socket.on("message-send", async (data) => {
+        const db = await open({filename: "./database/base.db", driver: sqlite3.Database});
+        await db.run(`INSERT INTO messages (userName, message) VALUES (?, ?)`, [data.userName, data.message]);
+        console.log(data.userName);
+        await db.close();
+        io.emit("message-response", {
+            message: data.message,
+            userName: data.userName
+        });
+    });
+});
+
+server.listen(3000);
